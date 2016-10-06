765004142d93a7301744578011c307e5564646c2
rparang <reza.parang@gmail.com>
2016-10-06 12:40:32 -0700

    added promo for free

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,9 +13,43 @@ class PagesController < ApplicationController
 
 	def product
 
+		# Params
 		@campaign = params[:c] || 'default'
+		@promotion = params[:promo] || nil
+		
+		# Text
 		@title = "The Humanize Blackhead Extractor | Humanize"
-		@copy = {
+		@default = landing_page_text[ 'default' ]
+		@header = landing_page_text[ @campaign ][ 'header' ] || @default[ 'header' ]
+		@description = landing_page_text[ @campaign ][ 'description' ] || @default[ 'description' ]
+
+		# Rails.logger.info( "promotion is #{@promotion}" )
+
+		# Promotion
+		if ( @promotion == "google" )
+			@promo = @promotion
+		end
+		
+
+	end
+
+	def checkout
+		@title = "Checkout | The Humanize Blackhead Extractor | Humanize"
+
+		# Promotion
+		@promotion = params[:promo] || nil
+
+		if ( @promotion == "google" )
+			@promo = @promotion
+		end
+
+	end
+
+  def buy
+  end
+
+  def landing_page_text
+  	{
 			'lip' => {
 				'header' => "Your blackhead's worst nightmare",
 				'description' => "Remove blackheads around your lip line with our professional blackhead extractor. The same tool used by your facialist."
@@ -29,20 +63,7 @@ class PagesController < ApplicationController
 				'description' => "Rid your face of unsightful blackheads with our Blackhead Extractor. The same tool used by your facialist."
 			}
 		}
-
-		@default = @copy[ 'default' ]
-		@header = @copy[ @campaign ][ 'header' ] || @default[ 'header' ]
-		@description = @copy[ @campaign ][ 'description' ] || @default[ 'description' ]
-
-		# Rails.logger.info( @header )
-		
-	end
-
-	def checkout
-		@title = "Checkout | The Humanize Blackhead Extractor | Humanize"
-	end
-
-  def buy
   end
+
 
 end
